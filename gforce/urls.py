aa4ccc61e6a8454105cd98752c5b75d40ee77a75
Gaurav Pore <poregaurav13@gmail.com>
2023-07-16 03:37:54 +0530

    Added Verify Otp Endpoint

--- gforce/urls.py
+++ gforce/urls.py
@@ -6,4 +6,5 @@ from .views import UserController
 urlpatterns = [
     path("signup-user", UserController.register_user, name="signup-new-user"),
     path("send-otp-email", UserController.send_email, name="send-otp-email"),
+    path("verify-otp", UserController.verify_otp, name="verify-otp"),
 ]
